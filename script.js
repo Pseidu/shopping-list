e768412a1d5c5e137a65355281fb9e6731c0a18a
Rafael Soriano <rsoriano.insti@gmail.com>
2023-06-09 19:49:14 +0200

    Borrada etiqueta /html que sobraba. Corregido js

--- script.js
+++ script.js
@@ -6,33 +6,65 @@ function inputLength() {
 	return input.value.length;
 }
 
-function addEventListenersToListElements()
-{
-	var liArray = document.querySelectorAll("li");
-	liArray.forEach(element => {
-		element.addEventListener("click",()=>element.classList.toggle("done"));
-	});
+// Así duplicaba los event handlers. Por eso no iba después de crear otro.
+// No pasa si en addEventListener utilizas una funcion NO anónima
+// function addEventListenersToListElements()
+// {
+// 	var liArray = document.querySelectorAll("li");
+// 	liArray.forEach(element => {
+// 		element.addEventListener("click",()=>element.classList.toggle("done"));
+// 	});
 	
-	var buttonsForDeletingItems = ul.querySelectorAll("button");
-	buttonsForDeletingItems.forEach(element => {
-		element.addEventListener("click",()=>element.parentElement.remove());
-	});
-}
+// 	var buttonsForDeletingItems = ul.querySelectorAll("button");
+// 	buttonsForDeletingItems.forEach(element => {
+// 		element.addEventListener("click",()=>element.parentElement.remove());
+// 	});
+// }
 
-function createListElement() {
-	// Create new li
+// function createListElement() {
+// 	// Create new li
+// 	var li = document.createElement("li");
+// 	li.appendChild(document.createTextNode(input.value + " "));
+// 	// Create button & append to li
+// 	var deleteButton = document.createElement("button");
+// 	deleteButton.appendChild(document.createTextNode("Delete"));
+// 	li.appendChild(deleteButton);
+// 	// Append li to list
+// 	ul.appendChild(li);
+// 	input.value = "";
+// 	// Add event listeners
+// 	addEventListenersToListElements();
+// }
+
+function addEventListenersToElement(element) {
+	element.addEventListener("click", () => {
+	  element.classList.toggle("done");
+	});
+  
+	var deleteButton = element.querySelector("button");
+	deleteButton.addEventListener("click", () => {
+	  element.remove();
+	});
+  }
+  
+  function addEventListenersToListElements() {
+	var liArray = document.querySelectorAll("li");
+	liArray.forEach(addEventListenersToElement);
+  }
+  
+  function createListElement() {
 	var li = document.createElement("li");
 	li.appendChild(document.createTextNode(input.value + " "));
-	// Create button & append to li
+  
 	var deleteButton = document.createElement("button");
 	deleteButton.appendChild(document.createTextNode("Delete"));
 	li.appendChild(deleteButton);
-	// Append li to list
+  
 	ul.appendChild(li);
 	input.value = "";
-	// Add event listeners
-	addEventListenersToListElements();
-}
+  
+	addEventListenersToElement(li);
+  }
 
 function addListAfterClick() {
 	if (inputLength() > 0) {
